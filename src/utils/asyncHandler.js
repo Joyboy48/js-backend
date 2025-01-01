2c23434ca104f81f048840a9aab910b84b5bdee5
Joyboy48 <astitvaarya9589@gmail.com>
2025-01-01 23:30:38 +0530

    commit

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,16 @@
-const asyncHandler = (requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
+// * A utility function to handle errors in asynchronous route handlers.
+// * This ensures that any errors thrown by async functions are caught
+// * and passed to the next middleware (typically an error handler).
 
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    // Ensure the requestHandler is executed and wrapped in a Promise
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    // If the Promise rejects, pass the error to the next middleware
+  };
+};
 
-export {asyncHandler}
+export { asyncHandler };
 
 // const asyncHandler = (func)=>async(req,res,next)=>{
 //     try {

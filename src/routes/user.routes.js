c33b2e4d94e16ad1c82a5168bceaa206d6e73637
Joyboy48 <astitvaarya9589@gmail.com>
2025-01-08 01:48:40 +0530

    feat: add update avatar endpoint to allow users to change their profile picture

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {logoutUser,loginUser,registerUser,refreshingAccessToken,changeCurrentPassword, getCurrentUser, updateAccountDetails} from "../controllers/user.controllers.js"
+import {logoutUser,loginUser,registerUser,refreshingAccessToken,changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar} from "../controllers/user.controllers.js"
 import {upload} from "../middlewares/multer.middlewares.js"
 import {verifyJWT} from "../middlewares/auth.middlewares.js"
 
@@ -31,6 +31,8 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/get-current-user").get(verifyJWT,getCurrentUser)
 
 router.route("/update-user-details").post(verifyJWT,updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
 
 
 export default router  
